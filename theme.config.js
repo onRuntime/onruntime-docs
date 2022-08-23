fc6a5ad37cf3255cacef9b4db33c79756c4b426a
Antoine Kingue <etorefr@gmail.com>
2022-08-23 22:09:04 +0200

    🔧 update theme head

--- theme.config.js
+++ theme.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
-import Head from "@components/Head";
 import Footer from "@components/Footer";
 import Brand from "@components/Brand";
+import urlcat from "urlcat";
 
 /**
  * @type {import("./theme").DocsThemeConfig}
@@ -12,13 +12,39 @@ const config = {
   titleSuffix: " | onRuntime Studio",
   projectChatLink: "https://discord.gg/ucX9c5yXmX",
   logo: <Brand />,
-  head: ({ title, meta }) => (
-    <Head
-      title={title}
-      description={meta.description}
-      keywords={meta.keywords}
-    />
-  ),
+  head: ({ title = "onRuntime Studio", meta }) => {
+    const description =
+      meta.description ||
+      "Studio that brings together teams of creators and develops applications, website and game platforms.";
+
+    const ogImage = urlcat(`https://og-image.onruntime.com/${title}.jpeg`, {
+      description,
+      theme: "dark",
+      md: 1,
+      fontSize: "100px",
+      images: "https://onruntime.com/static/images/logo/onruntime.svg",
+    });
+
+    return (
+      <>
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta name="description" content={description} />
+        <meta name="og:description" content={description} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@onRuntime" />
+        <meta name="twitter:image" content={ogImage} />
+        <meta
+          name="og:title"
+          content={title ? title + " | onRuntime" : "onRuntime Studio"}
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={description} />
+        <meta name="og:title" content={title} />
+        <meta name="og:image" content={ogImage} />
+      </>
+    );
+  },
   search: true,
   defaultMenuCollapsed: true,
   prevLinks: true,
