2fa10346ab4f47ce8520034c46f4dc699f097b20
Antoine Kingue <contact@antoinek.fr>
2024-01-14 01:25:01 +0100

    🚨 fix lint errors

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,7 @@ import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static async getInitialProps(
-    ctx: DocumentContext
+    ctx: DocumentContext,
   ): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
